65384d7c72fbabe2282f7891188af19a62b677fa
Drokewood <hermes.alex87@gmail.com>
2025-11-03 16:23:29 +0100

    feat: setup Instagram gallery feature branch
    - Create InstagramGallery component scaffold
    - Update TODO with current sprint planning
    - Prepare for Instagram API integration (Phase 1 Must-Have)

---
+++ src/components/InstagramGallery.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const GalleryContainer = styled.div`
+  /* Instagram Gallery Styles - Coming Soon */
+`;
+
+const InstagramGallery = () => {
+  return (
+    <GalleryContainer>
+      <h2>Instagram Gallery</h2>
+      <p>Instagram integration coming soon...</p>
+      {/* TODO: Implement Instagram API integration */}
+    </GalleryContainer>
+  );
+};
+
+export default InstagramGallery;
